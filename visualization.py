addeb43566427b9287229c177a06351ad5046688
Grzetan <grzetan@gmail.com>
2023-05-30 14:43:57 +0200

    Updated visualization script

--- visualization.py
+++ visualization.py
@@ -14,24 +14,23 @@ for line in open(path, 'r'):
     points.append([float(c[0]), float(c[1]), float(c[2])])
 
 # Delete all objects
-bpy.ops.object.mode_set(mode='OBJECT')
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.object.delete(use_global=False)
+#bpy.ops.object.mode_set(mode='OBJECT')
+#bpy.ops.object.select_all(action='SELECT')
+#bpy.ops.object.delete(use_global=False)
 
-# Add new path
-cu = bpy.data.curves.new("Path", "CURVE")
-cu.dimensions = '3D'
-ob = bpy.data.objects.new("Path", cu)
-polyline = cu.splines.new('BEZIER')  # 'POLY''BEZIER''BSPLINE''CARDINAL''NURBS'
-bpy.context.collection.objects.link(ob)
+crv = bpy.data.curves.new('crv', 'CURVE')
+crv.dimensions = '3D'
 
-bpy.context.view_layer.objects.active = ob
+# make a new spline in that curve
+spline = crv.splines.new(type='POLY')
 
-bpy.ops.object.mode_set(mode='EDIT')
+# a spline point for each point
+spline.points.add(len(points)-1) # theres already one point by default
 
-for i, p in enumerate(points):
-    
-    bpy.ops.curve.vertex_add(location=p)
-    
+# assign the point coordinates to the spline points
+for p, new_co in zip(spline.points, points):
+    p.co = (new_co + [1.0]) # (add nurbs weight)
 
-bpy.ops.object.mode_set(mode='OBJECT')
+# make a new object with the curve
+obj = bpy.data.objects.new('object_name', crv)
+bpy.context.collection.objects.link(obj)
